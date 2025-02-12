438ee4e79d9ba7d5b7cecb4f51d7c612700cdd23
kngval <bkvalteocs@gmail.com>
2025-02-12 17:19:53 +0800

    footer/copyright

---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <div>{"<Brent KV/>"}</div>
+      <div className="copyright">Images sourced from Goodreads. All rights belong to their respective owners.</div>
+    </footer>
+  )
+}
+
+export default Footer;
