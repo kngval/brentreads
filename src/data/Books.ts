3e13919d9bdac55ef973bd048b7dffab2ec291a1
kngval <bkvalteocs@gmail.com>
2025-04-07 11:52:19 +0800

    Pretty Girls - Karin Slaughter

--- src/data/Books.ts
+++ src/data/Books.ts
@@ -1,10 +1,10 @@
-import { BookEntity }  from "./BookEntity";
+import { BookEntity } from "./BookEntity";
 export const books: BookEntity[] = [
   new BookEntity(
     "The Last Time I Lied",
     "Fifteen years ago, summer camper Emma Davis watched sleepily as her three cabin mates snuck out of their cabin in the dead of night. The last she--and anyone--saw of them was Vivian closing the cabin door behind her, hushing Emma with a finger pressed to her lips.",
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1511141004i/36626748.jpg"
   ),
@@ -12,7 +12,7 @@ export const books: BookEntity[] = [
     "The Silent Patient",
     "Alicia Berenson’s life is seemingly perfect. A famous painter married to an in-demand fashion photographer, she lives in a grand house with big windows overlooking a park in one of London’s most desirable areas. One evening her husband Gabriel returns home late from a fashion shoot, and Alicia shoots him five times in the face, and then never speaks another word.",
     "Alex Michaelides",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1668782119i/40097951.jpg"
   ),
@@ -21,7 +21,7 @@ export const books: BookEntity[] = [
     `Now reduced to a schoolyard chant, the Hope family murders shocked the Maine coast one bloody night in 1929. While most people assume seventeen-year-old Lenora was responsible, the police were never able to prove it. Other than her denial after the killings, she has never spoken publicly about that night, nor has she set foot outside Hope’s End, the cliffside mansion where the massacre occurred.
 `,
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1668181003l/62703226.jpg"
   ),
@@ -29,26 +29,11 @@ export const books: BookEntity[] = [
     "Rock Paper Scissors",
     `Things have been wrong with Mr and Mrs Wright for a long time. When Adam and Amelia win a weekend away to Scotland, it might be just what their marriage needs. Self-confessed workaholic and screenwriter Adam Wright has lived with face blindness his whole life. He can’t recognize friends or family, or even his own wife.`,
     "Alice Feeney",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1612020952l/56269064.jpg"
   ),
-  new BookEntity(
-    "False Witness",
-`Karin Slaughter’s False Witness is a gripping psychological thriller that follows Leigh Collier, a successful defense attorney with a seemingly stable life. When she is unexpectedly assigned to defend a wealthy client accused of a brutal crime, Leigh realizes he knows about a dark secret from her past—one she and her estranged sister, Callie, have desperately tried to forget. As the case unfolds, Leigh must confront long-buried trauma, navigate complex moral dilemmas, and protect the life she has built while facing the consequences of a past she thought was behind her. Slaughter weaves a tense and emotional narrative, blending legal suspense with deep character exploration.`,
-    "Karin Slaughter",
-    ["Mystery","Thriller"],
-    5,
-    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1615850400l/55809780.jpg"
-  ),
-  new BookEntity(
-    "The Guest List",
-    `On an island off the coast of Ireland, guests gather to celebrate two people joining their lives together as one. The groom: handsome and charming, a rising television star. The bride: smart and ambitious, a magazine publisher. It’s a wedding for a magazine, or for a celebrity: the designer dress, the remote location, the luxe party favors, the boutique whiskey. The cell phone service may be spotty and the waves may be rough, but every detail has been expertly planned and will be expertly executed.`,
-    "Lucy Foley",
-    ["Mystery","Thriller"],
-    4,
-    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1591461181i/52656911.jpg"
-  ),
+
   new BookEntity(
     "Gone Girl",
     `Who are you?
@@ -57,36 +42,64 @@ These are the questions Nick Dunne finds himself asking on the morning of his fi
 
 So what did happen to Nick's beautiful wife?`,
     "Gillian Flynn",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
-   "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554086139l/19288043.jpg" 
+    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554086139l/19288043.jpg"
+  ),
+ new BookEntity(
+    "Pretty Girls",
+    `Twenty years ago Claire Scott's eldest sister, Julia, went missing. No one knew where she went - no note, no body. It was a mystery that was never solved and it tore her family apart.
+
+Now another girl has disappeared, with chilling echoes of the past. And it seems that she might not be the only one.
+
+Claire is convinced Julia's disappearance is linked.
+
+But when she begins to learn the truth about her sister, she is confronted with a shocking discovery, and nothing will ever be the same...`,
+    "Karin Slaughter",
+    ["Mystery", "Thriller"],
+    4,
+    "https://images.squarespace-cdn.com/content/v1/645cfd444d94b7698271dee9/1683815793679-3HEMAEI10K566MO7X16D/PRETTY+GIRLS+UK.jpg"
+  ),
+  new BookEntity(
+    "The Guest List",
+    `On an island off the coast of Ireland, guests gather to celebrate two people joining their lives together as one. The groom: handsome and charming, a rising television star. The bride: smart and ambitious, a magazine publisher. It’s a wedding for a magazine, or for a celebrity: the designer dress, the remote location, the luxe party favors, the boutique whiskey. The cell phone service may be spotty and the waves may be rough, but every detail has been expertly planned and will be expertly executed.`,
+    "Lucy Foley",
+    ["Mystery", "Thriller"],
+    4,
+    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1591461181i/52656911.jpg"
   ),
 
-new BookEntity(
+  new BookEntity(
     "None of this is true",
     `Celebrating her forty-fifth birthday at her local pub, popular podcaster Alix Summers crosses paths with an unassuming woman called Josie Fair. Josie, it turns out, is also celebrating her forty-fifth birthday. They are, in fact, birthday twins.`,
     "Lisa Jewell",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
-   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1686092508i/62334530.jpg" 
+    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1686092508i/62334530.jpg"
+  ),
+  new BookEntity(
+    "False Witness",
+    `Karin Slaughter’s False Witness is a gripping psychological thriller that follows Leigh Collier, a successful defense attorney with a seemingly stable life. When she is unexpectedly assigned to defend a wealthy client accused of a brutal crime, Leigh realizes he knows about a dark secret from her past—one she and her estranged sister, Callie, have desperately tried to forget. As the case unfolds, Leigh must confront long-buried trauma, navigate complex moral dilemmas, and protect the life she has built while facing the consequences of a past she thought was behind her. Slaughter weaves a tense and emotional narrative, blending legal suspense with deep character exploration.`,
+    "Karin Slaughter",
+    ["Mystery", "Thriller"],
+    4,
+    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1615850400l/55809780.jpg"
   ),
 
   new BookEntity(
     "The Girl On The Train",
     `Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. She’s even started to feel like she knows the people who live in one of the houses. “Jess and Jason,” she calls them. Their life—as she sees it—is perfect. If only Rachel could be that happy. And then she sees something shocking. It’s only a minute until the train moves on, but it’s enough. Now everything’s changed. Now Rachel has a chance to become a part of the lives she’s only watched from afar. Now they’ll see; she’s much more than just the girl on the train...`,
     "Paula Hawkins",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
-   "https://thischickreads.com/wp-content/uploads/2024/11/71vWnStCnEL._AC_UF8941000_QL80_.jpg" 
+    "https://thischickreads.com/wp-content/uploads/2024/11/71vWnStCnEL._AC_UF8941000_QL80_.jpg"
   ),
-new BookEntity(
+  new BookEntity(
     "Lock Every Door",
     `No visitors. No nights spent away from the apartment. No disturbing the other residents, all of whom are rich or famous or both. These are the only rules for Jules Larsen's new job as an apartment sitter at the Bartholomew, one of Manhattan's most high-profile and mysterious buildings. Recently heartbroken and just plain broke, Jules is taken in by the splendor of her surroundings and accepts the terms, ready to leave her past life behind.`,
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     3,
-   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1540938359i/41837243.jpg" 
+    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1540938359i/41837243.jpg"
   ),
-
-] 
-
+];
