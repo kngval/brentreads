7395203897df578985ca040910b95686a3a5240d
kngval <bkvalteocs@gmail.com>
2026-06-05 19:20:56 -0700

    the shining & thohh

--- src/data/Books.ts
+++ src/data/Books.ts
@@ -1,10 +1,10 @@
-import { BookEntity }  from "./BookEntity";
+import { BookEntity } from "./BookEntity";
 export const books: BookEntity[] = [
   new BookEntity(
     "The Last Time I Lied",
     "Fifteen years ago, summer camper Emma Davis watched sleepily as her three cabin mates snuck out of their cabin in the dead of night. The last she--and anyone--saw of them was Vivian closing the cabin door behind her, hushing Emma with a finger pressed to her lips.",
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1511141004i/36626748.jpg"
   ),
@@ -12,7 +12,7 @@ export const books: BookEntity[] = [
     "The Silent Patient",
     "Alicia Berenson’s life is seemingly perfect. A famous painter married to an in-demand fashion photographer, she lives in a grand house with big windows overlooking a park in one of London’s most desirable areas. One evening her husband Gabriel returns home late from a fashion shoot, and Alicia shoots him five times in the face, and then never speaks another word.",
     "Alex Michaelides",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1668782119i/40097951.jpg"
   ),
@@ -21,7 +21,7 @@ export const books: BookEntity[] = [
     `Now reduced to a schoolyard chant, the Hope family murders shocked the Maine coast one bloody night in 1929. While most people assume seventeen-year-old Lenora was responsible, the police were never able to prove it. Other than her denial after the killings, she has never spoken publicly about that night, nor has she set foot outside Hope’s End, the cliffside mansion where the massacre occurred.
 `,
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1668181003l/62703226.jpg"
   ),
@@ -29,7 +29,7 @@ export const books: BookEntity[] = [
     "Rock Paper Scissors",
     `Things have been wrong with Mr and Mrs Wright for a long time. When Adam and Amelia win a weekend away to Scotland, it might be just what their marriage needs. Self-confessed workaholic and screenwriter Adam Wright has lived with face blindness his whole life. He can’t recognize friends or family, or even his own wife.`,
     "Alice Feeney",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     5,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1612020952l/56269064.jpg"
   ),
@@ -37,7 +37,7 @@ export const books: BookEntity[] = [
     "Sharp Objects",
     `Fresh from a brief stay at a psych hospital, reporter Camille Preaker faces a troubling assignment: she must return to her tiny hometown to cover the unsolved murder of a preteen girl and the disappearance of another. For years, Camille has hardly spoken to her neurotic, hypochondriac mother or to the half-sister she barely knows: a beautiful thirteen-year-old with an eerie grip on the town. Now, installed in her old bedroom in her family's Victorian mansion, Camille finds herself identifying with the young victims—a bit too strongly. Dogged by her own demons, she must unravel the psychological puzzle of her own past if she wants to get the story—and survive this homecoming.`,
     "Gillian Flynn",
-    ["Mystery","Thriller","Horror"],
+    ["Mystery", "Thriller", "Horror"],
     5,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1475695315i/18045891.jpg"
   ),
@@ -45,7 +45,7 @@ export const books: BookEntity[] = [
     "The Guest List",
     `On an island off the coast of Ireland, guests gather to celebrate two people joining their lives together as one. The groom: handsome and charming, a rising television star. The bride: smart and ambitious, a magazine publisher. It’s a wedding for a magazine, or for a celebrity: the designer dress, the remote location, the luxe party favors, the boutique whiskey. The cell phone service may be spotty and the waves may be rough, but every detail has been expertly planned and will be expertly executed.`,
     "Lucy Foley",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
     "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1591461181i/52656911.jpg"
   ),
@@ -57,42 +57,42 @@ These are the questions Nick Dunne finds himself asking on the morning of his fi
 
 So what did happen to Nick's beautiful wife?`,
     "Gillian Flynn",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
-   "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554086139l/19288043.jpg" 
+    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554086139l/19288043.jpg"
   ),
 
-new BookEntity(
+  new BookEntity(
     "None of this is true",
     `Celebrating her forty-fifth birthday at her local pub, popular podcaster Alix Summers crosses paths with an unassuming woman called Josie Fair. Josie, it turns out, is also celebrating her forty-fifth birthday. They are, in fact, birthday twins.`,
     "Lisa Jewell",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     4,
-   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1686092508i/62334530.jpg" 
+    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1686092508i/62334530.jpg"
   ),
 
-new BookEntity(
+  new BookEntity(
     "Lock Every Door",
     `No visitors. No nights spent away from the apartment. No disturbing the other residents, all of whom are rich or famous or both. These are the only rules for Jules Larsen's new job as an apartment sitter at the Bartholomew, one of Manhattan's most high-profile and mysterious buildings. Recently heartbroken and just plain broke, Jules is taken in by the splendor of her surroundings and accepts the terms, ready to leave her past life behind.`,
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     3,
-   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1540938359i/41837243.jpg" 
+    "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1540938359i/41837243.jpg"
   ),
   new BookEntity(
     "The Girl On The Train",
     `Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. She’s even started to feel like she knows the people who live in one of the houses. “Jess and Jason,” she calls them. Their life—as she sees it—is perfect. If only Rachel could be that happy. And then she sees something shocking. It’s only a minute until the train moves on, but it’s enough. Now everything’s changed. Now Rachel has a chance to become a part of the lives she’s only watched from afar. Now they’ll see; she’s much more than just the girl on the train...`,
     "Paula Hawkins",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     3,
-   "https://thischickreads.com/wp-content/uploads/2024/11/71vWnStCnEL._AC_UF8941000_QL80_.jpg" 
+    "https://thischickreads.com/wp-content/uploads/2024/11/71vWnStCnEL._AC_UF8941000_QL80_.jpg"
   ),
 
   new BookEntity(
     "False Witness",
-`Karin Slaughter’s False Witness is a gripping psychological thriller that follows Leigh Collier, a successful defense attorney with a seemingly stable life. When she is unexpectedly assigned to defend a wealthy client accused of a brutal crime, Leigh realizes he knows about a dark secret from her past—one she and her estranged sister, Callie, have desperately tried to forget. As the case unfolds, Leigh must confront long-buried trauma, navigate complex moral dilemmas, and protect the life she has built while facing the consequences of a past she thought was behind her. Slaughter weaves a tense and emotional narrative, blending legal suspense with deep character exploration.`,
+    `Karin Slaughter’s False Witness is a gripping psychological thriller that follows Leigh Collier, a successful defense attorney with a seemingly stable life. When she is unexpectedly assigned to defend a wealthy client accused of a brutal crime, Leigh realizes he knows about a dark secret from her past—one she and her estranged sister, Callie, have desperately tried to forget. As the case unfolds, Leigh must confront long-buried trauma, navigate complex moral dilemmas, and protect the life she has built while facing the consequences of a past she thought was behind her. Slaughter weaves a tense and emotional narrative, blending legal suspense with deep character exploration.`,
     "Karin Slaughter",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     3,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1615850400l/55809780.jpg"
   ),
@@ -100,9 +100,36 @@ new BookEntity(
     "The House Across The Lake",
     "Casey Fletcher, a recently widowed actress trying to escape a streak of bad press, has retreated to the peace and quiet of her family’s lake house in Vermont. Armed with a pair of binoculars and several bottles of liquor, she passes the time watching Tom and Katherine Royce, the glamorous couple who live in the house across the lake. They make for good viewing—a tech innovator, Tom is rich; and a former model, Katherine is gorgeous. ",
     "Riley Sager",
-    ["Mystery","Thriller"],
+    ["Mystery", "Thriller"],
     2,
     "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1639618949l/58909880.jpg"
   ),
-] 
+  new BookEntity(
+    "The Shining",
+    `"Danny is only five years old but in the words of Old Mr Hallorann he is a ‘shiner,’ aglow with psychic voltage. When his father becomes caretaker of the Overlook hotel, Danny’s visions grow out of control.
+
+As winter closes in and blizzards cut them off, the hotel seems to develop a life of its own. It is meant to be empty. So who is the lady in Room 217 and who are the masked guests going up and down the elevator? And why do the hedges shaped like animals seem so alive?
+
+Somewhere, somehow, there is an evil force in the hotel – and that too is beginning to shine.
+`,
+    "Stephen King",
+    ["Horror", "Psychological", "Thriller"],
+    4,
+    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1353277730i/11588.jpg"
+
+  ),
+
+  new BookEntity(
+    "The Haunting of Hill House",
+    `“Hill House, not sane, stood by itself against its hills, holding darkness within. . . .”
+
+Four seekers have arrived at the rambling old pile known as Hill House: Dr. Montague, an occult scholar looking for solid evidence of psychic phenomena; Theodora, his lovely and lighthearted assistant; Luke, the adventurous future inheritor of the estate; and Eleanor, a friendless, fragile young woman with a dark past. As they begin to cope with chilling, even horrifying occurrences beyond their control or understanding, they cannot possibly know what lies ahead. For Hill House is gathering its powers—and soon it will choose one of them to make its own..
+`,
+    "Stephen King",
+    ["Horror", "Psychological", "Thriller"],
+    4,
+    "https://images.penguinrandomhouse.com/cover/9780143129370"
+
+  ),
+]
 
